aa6b6b66aadadd305f4eda744aa1f435381e8474
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-11-09 23:56:03 -0800

    Add search input to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 // import { Link } from "react-router-dom";
-import { Toolbar, Typography, Button } from "@mui/material";
+import { Toolbar, Typography, Button, InputBase } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 
 function Navbar() {
@@ -9,6 +9,7 @@ function Navbar() {
         <Typography variant="h5" sx={{ flexGrow: 1 }}>
           PickleHub
         </Typography>
+        <InputBase placeholder="Search…" />
         <Button color="inherit">Create New Post</Button>
       </Toolbar>
     </AppBar>
